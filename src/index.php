1543769cc726de2f08fa4704ced58f6f83067ec2
7472062 <7472062@gmail.com>
2025-05-13 21:03:37 +0900

    폰트, 타이틀 등 메인 페이지 기본 세팅

--- src/index.php
+++ src/index.php
@@ -1,3 +1,24 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>게시판</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1><a href="index.php">게시판</h1>
+            <nav class="top-nav">
+                <ul>
+                    <li><a href="login.php">로그인</a></li>
+                    <li><a href="register.php">회원가입</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+
+</html>
