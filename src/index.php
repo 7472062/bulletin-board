ebe6cbfa549310aa02cd7876e7e6a01c9ee4b87c
7472062 <7472062@gmail.com>
2025-05-16 10:58:36 +0900

    게시글 리스트 출력 코드 작성

--- src/index.php
+++ src/index.php
@@ -27,11 +27,40 @@ require_once 'db_conn.php';
         </header>
         <main>
             <div class="post-button">
-                <a href="write_post.php">+</a>
+                <a href="write_post.php">글쓰기</a>
             </div>
+            <table class="post-list">
+                <thead>
+                    <tr>
+                        <th class="post-id">번호</th>
+                        <th class="post-title-header">제목</th>
+                        <th class="post-user">작성자</th>
+                        <th class="post-date">작성일</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT p.id, p.title, p.created_at, u.username 
+                              FROM posts p
+                              JOIN users u ON p.user_id = u.id
+                              ORDER BY p.created_at DESC";
+                    $result = $conn->query($query);
+                    if ($result->num_rows) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td class='post-id'>" . $row['id'] . "</td>";
+                            echo "<td class='post-title'><a href='view_post.php?id=" . $row['id'] . "'>" . $row['title'] . "</a></td>";
+                            echo "<td class='post-user'>" . $row['username'] . "</td>";
+                            echo "<td class='post-date'>" . date("Y/m/d", strtotime($row['created_at'])) . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='4' style='text-align: center;'>등록된 게시글이 없습니다.</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </main>
-
-
     </div>
 </body>
 </html>
